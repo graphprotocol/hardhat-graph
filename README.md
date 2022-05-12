246ebaae691241f42b20db71025dcb2e933f2409
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-05-12 16:13:02 +0300

    Add init and update (#5)
    * Update gitignore
    
    * Add init task
    
    * Add postinstall script
    
    * Try another script
    
    * Update package.json
    
    * Add missing comma
    
    * Initial update subtask version
    
    * Add spinner to update
    
    * deps
    
    * deps again
    
    * try peer deps
    
    * Add a graph task. Use either hre.network.name or hre.config.defaultNetwork. Update network config if network not present
    
    * Update the check if git repo exists
    
    * Make subgraph product, name and folder configurable
    
    * Use same naming convetion
    
    * Make indexEvents configurable
    
    * Fix config issues
    
    * update readme
    
    * Fix formatting

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # Hardhat-graph
+
+## `init` subtask:
+  - Expects two string parameters: `contract: 'contractName'` and `address: '0x123..`
+  - Workflow:
+    - Generates a subgraph in `./subgraph` using `generateScaffold` from `graph-cli`
+    - Generates a network.json file in `./subgraph` using `initNetworksConfig` from `graph-cli`
+    - Initialises a new repo if one does not currently exist. (Currently it does not create an initial commit)
+    - Generates or updates an existing .gitignore file.
+    - Runs `codegen` command
+  - Example usage:
+```typescript
+async function deploy(contractName: string) {
+  ....
+  await contract.deployed();
+  return { contract: contractName , address: contract.address}
+}
+
+deploy()
+  .then((result) => hre.run('init', result))
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+```
+
+## `update` subtask:
+  - Expects two string parameters: `contract: 'contractName'` and `address: '0x123..`
+  - Workflow:
+    - Updates the contract ABI in `./subgraph/abis`
+    - Updates the contract Address in `network.json` if it's deployed to the same network. If the contract has been deployed to a network that is not present in the config file, adds an entry for the new network.
+    - Checks for changes to the contract events. If there are any changes the task will exit and the user will be informed and prompted to address the changes in the subgraph.yaml file and manually run `codegen` and `build`.
+    - Runs `codegen` and `build` commands if there are no changes to the contract events.
+  - Example usage:
+```typescript
+async function deploy(contractName: string) {
+  ....
+  await contract.deployed();
+  return { contract: contractName , address: contract.address}
+}
+
+deploy()
+  .then((result) => hre.run('update', result))
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+```
+
+## `graph` subtask:
+  - Expects two string parameters: `contract: 'contractName'` and `address: '0x123..` and an optional positional parameter `subtask` <init|update>.
+  - Workflow:
+    - Conditionally runs either `init` or `update` subtask depending if a subgraph already exists or not. If the optional param `subtask` is passed it will run that subtask instead.
+  - Example usage:
+```typescript
+async function deploy(contractName: string) {
+  ....
+  await contract.deployed();
+  return { contract: contractName , address: contract.address}
+}
+
+deploy()
+  .then((result) => hre.run('graph', result))
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+```
+or
+```sh
+npx hardhat graph <init|update> --contract ContractName --address 0x123... # the subtask parameter is optional
+```
+
+## How to try it out:
+NOTE:
+npm >7 should auto-install peerDependencies from plugins, but if they are not or you're using `yarn`, add
+```
+"@graphprotocol/graph-cli": "^0.29.0",
+"@graphprotocol/graph-ts": "^0.26.0"
+```
+to the hardhat project package.json
+
+The plugin can be installed from the branch:
+```
+"hardhat-graph": "https://github.com/graphprotocol/hardhat-graph#add-init-and-update"
+```
+Import the plugin in your `hardhat.config` file:
+JS: `require('hardhat-graph')`
+TS: `import 'hardhat-graph'`
+
+## Configurable options in hardhat.config file:
+
+```typescript
+module.exports = {
+  ...
+  subgraph: {
+    name: 'MySubgraph', // Defaults to the name of the root folder of the hardhat project
+    product: 'hosted-service'|'subgraph-studio', // Defaults to 'subgraph-studio'
+    indexEvents: true|false // Defaults to false
+  },
+  paths: {
+    subgraph: './path/to/subgraph' // Defaults to './subgraph'
+  }
+}
+```
+
+## Issues to resolve:
+
+- [ ] Instead of subtask `init` and `update` could be functions, this potentially could make them easier to integrated in variety of custom workflows
+- [ ] `graph` task could be renamed
+- [ ]  Include the `add` subtask/command/function to the `graph` task.
+- [ ]  Add an option to auto-resolve events changes.
+- [ ] Is it okay to create an initial commit when initialising a repo? We would not want to commit hardhat files that otherwise should not be committed
+- [ ] Properly set peerDeps versions
---
+++ src/tasks.ts
@@ -0,0 +1,147 @@
+import { subtask, task } from 'hardhat/config'
+import { initSubgraph, initRepository, initGitignore, runCodegen, runBuild, checkForRepo, getEvents, eventsDiff, updateNetworksFile } from './task-helpers'
+import * as toolbox from 'gluegun'
+import * as YAML from 'yaml'
+import path from 'path'
+import immutable from 'immutable'
+import { ethers } from 'ethers'
+import { spawn } from 'child_process'
+const { withSpinner, step } = require('@graphprotocol/graph-cli/src/command-helpers/spinner')
+const { initNetworksConfig } = require('@graphprotocol/graph-cli/src/command-helpers/network')
+
+task("graph", "A do all task")
+  .addOptionalPositionalParam("subtask", "Specify which subtask to execute")
+  .addParam("contract", "The name of the contract")
+  .addParam("address", "The address of the contract")
+  .setAction(async (taskArgs, hre) => {
+      let directory = hre.config.paths.subgraph
+      let subgraph = toolbox.filesystem.exists(directory) == "dir" && toolbox.filesystem.exists(path.join(directory, 'subgraph.yaml')) == "file"
+      let command = subgraph ? "update" : "init"
+      let { subtask, ...args } = taskArgs
+
+      await hre.run(subtask || command, args)
+  });
+
+
+/// MAYBE INIT AND UPDATE SHOULD NOT BE SUBTASKS BUT JUST FUNCTIONS?
+subtask("init", "Initialize a subgraph")
+  .addParam("contract", "The name of the contract")
+  .addParam("address", "The address of the contract")
+  .setAction(async (taskArgs, hre) => {
+    const directory = hre.config.paths.subgraph
+
+    if(toolbox.filesystem.exists(directory) == "dir" && toolbox.filesystem.exists(path.join(directory, 'subgraph.yaml')) == "file") {
+      toolbox.print.error("Subgraph already exists! Please use the update subtask to update an existing subgraph!")
+      process.exit(1)
+    }
+
+    let scaffold = await initSubgraph(taskArgs, hre)
+    if (scaffold !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    let networkConfig = await initNetworksConfig(toolbox, directory, 'address')
+    if (networkConfig !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    let isGitRepo = await checkForRepo(toolbox)
+    if (!isGitRepo) {
+      let repo = await initRepository(toolbox)
+      if (repo !== true) {
+        process.exitCode = 1
+        return
+      }
+    }
+
+    // Maybe Not needed?
+    let gitignore = await initGitignore(toolbox)
+    if (gitignore !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    let codegen = await runCodegen(directory)
+    if (codegen !== true) {
+      process.exitCode = 1
+      return
+    }
+  })
+
+subtask("update", "Updates an existing subgraph from artifact or contract address")
+  .addParam("contract", "The name of the contract")
+  .addParam("address", "The address of the contract")
+  .setAction(async (taskArgs: any, hre) => {
+    const directory = hre.config.paths.subgraph
+    const network = hre.network.name || hre.config.defaultNetwork
+    const subgraph = await toolbox.filesystem.read(path.join(directory, 'subgraph.yaml'), 'utf8')
+
+    if (!toolbox.filesystem.exists(directory) || !subgraph) {
+      toolbox.print.error("No subgraph found! Please first initialize a new subgraph!")
+      process.exit(1)
+    }
+
+    await withSpinner(
+      `Update subgraph`,
+      `Failed to update subgraph`,
+      `Warnings while updating subgraph`,
+      async (spinner: any) => {
+        // New contract version
+        step(spinner, `Fetching new contract version`)
+        let contract = await hre.artifacts.readArtifact(taskArgs.contract)
+
+        // Old contract version
+        step(spinner, `Fetching current contract version from subgraph`)
+        let manifest = YAML.parse(subgraph)
+        let dataSource = manifest.dataSources.find((source: { source: { abi: { name: string } } }) => source.source.abi == taskArgs.contract)
+        let subgraphAbi = dataSource.mapping.abis.find((abi: { name: string }) => abi.name == taskArgs.contract)
+        let abiJson = await toolbox.filesystem.read(path.join(directory, subgraphAbi.file))
+
+        if (!abiJson) {
+          toolbox.print.error(`Could not read ${path.join(directory, subgraphAbi.file)}`)
+          process.exit(1)
+        }
+        // Convert to Interface
+        let newAbi = new ethers.utils.Interface(contract.abi)
+        let currentAbi = new ethers.utils.Interface(abiJson)
+
+        // Fetch new events from Interface
+        let newAbiEvents = await getEvents(newAbi)
+        // Fetch current dataSource events from subgraph.yaml
+        let currentAbiEvents = dataSource.mapping.eventHandlers.map((handler: { event: string }) => { return handler.event })
+        let newEvents = await eventsDiff(newAbiEvents, currentAbiEvents)
+        let removedEvents = await eventsDiff(currentAbiEvents, newAbiEvents)
+
+        // Update the subgraph ABI
+        step(spinner, `Updating contract ABI in subgraph`)
+        await toolbox.patching.update(path.join(directory, subgraphAbi.file), (abi: any) => {
+          return contract.abi
+        })
+
+        step(spinner, `Updating contract's ${network} address in networks.json`)
+        await updateNetworksFile(toolbox, network, dataSource.name, taskArgs.address, directory)
+
+        step(spinner, `Checking for changes to the contract events`)
+        if(newAbiEvents.length != currentAbiEvents.length || newEvents.length != 0 || removedEvents.length != 0) {
+          toolbox.print.warning(
+            `Contract events have been changed!\nCurrent events:\n ${currentAbiEvents.join('\n ')}\nNew events:\n ${newAbiEvents.join('\n ')}\nPlease address the change in your subgraph.yaml and run graph codegen and graph build from the subgraph folder!`
+          )
+        } else {
+          let codegen = await runCodegen(directory)
+          if (codegen !== true) {
+            process.exit(1)
+            return
+          }
+
+          let build = await runBuild(network, directory)
+          if (build !== true) {
+            process.exitCode = 1
+            return
+          }
+        }
+        return true
+      }
+    )
+  })
