2a6c1fd80d05736a5f93068c7bbba2ecefbb87d9
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-05-12 18:26:48 +0300

    Reorganize the code

---
+++ src/helpers/events.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'ethers'
+
+export const compareAbiEvents = async(toolbox: any, dataSource: any, newAbiJson: any, currentAbiJson: any): Promise<boolean> => {
+  // Convert to Interface
+  let newAbi = new ethers.utils.Interface(newAbiJson)
+  // Get events signatures
+  let newAbiEvents = Object.keys(newAbi.events)
+  // Fetch current dataSource events signatures from subgraph.yaml
+  let currentAbiEvents = dataSource.mapping.eventHandlers.map((handler: { event: string }) => { return handler.event })
+  // Check for renamed or replaced events
+  let changedEvents = await eventsDiff(currentAbiEvents, newAbiEvents)
+  // let removedEvents = await eventsDiff(currentAbiEvents, newAbiEvents)
+
+  let changed = newAbiEvents.length != currentAbiEvents.length || changedEvents.length != 0
+
+  if(changed) {
+    toolbox.print.warning(
+      `Contract events have been changed!\n
+      Current events:\n
+      ${currentAbiEvents.join('\n')}\n
+      New events:\n ${newAbiEvents.join('\n')}\n
+      Please address the change in your subgraph.yaml and run graph codegen and graph build from the subgraph folder!`.replace(' ', '')
+    )
+  }
+
+  return changed
+}
+
+const eventsDiff = async (array1: string[], array2: string[]): Promise<string[]> => {
+  return array1.filter(x => !array2.includes(x))
+}
---
+++ src/helpers/subgraph.ts
@@ -0,0 +1,69 @@
+import path from 'path'
+import immutable from 'immutable'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const graphCli = require('@graphprotocol/graph-cli/src/cli')
+const Protocol = require('@graphprotocol/graph-cli/src/protocols')
+const { chooseNodeUrl } = require('@graphprotocol/graph-cli/src/command-helpers/node')
+const { withSpinner, step } = require('@graphprotocol/graph-cli/src/command-helpers/spinner')
+const { generateScaffold, writeScaffold } = require('@graphprotocol/graph-cli/src/command-helpers/scaffold')
+
+export const initSubgraph = async (taskArgs: { contract: string, address: string }, hre: HardhatRuntimeEnvironment): Promise<boolean> =>
+  await withSpinner(
+    `Create subgraph scaffold`,
+    `Failed to create subgraph scaffold`,
+    `Warnings while creating subgraph scaffold`,
+    async (spinner: any) => {
+      let protocolInstance = new Protocol('ethereum')
+      let ABI = protocolInstance.getABI()
+
+      let contract = await hre.artifacts.readArtifact(taskArgs.contract)
+      let abi = new ABI(contract.contractName, undefined, immutable.fromJS(contract.abi))
+      let product = hre.config?.subgraph?.product || 'subgraph-studio'
+      let { node, allowSimpleName } = chooseNodeUrl({
+        product: product,
+        studio: undefined,
+        node: undefined,
+        allowSimpleName: true
+      })
+
+      let scaffold = await generateScaffold(
+        {
+          protocolInstance,
+          network: hre.network.name || hre.config.defaultNetwork,
+          subgraphName: hre.config.subgraph?.name || path.basename(hre.config.paths.root),
+          abi,
+          contract: taskArgs.address,
+          contractName: contract.contractName,
+          indexEvents: hre.config.subgraph?.indexEvents,
+          node,
+        },
+        spinner,
+      )
+
+      await writeScaffold(scaffold, hre.config.paths.subgraph, spinner)
+
+      return true
+    }
+  )
+
+export const updateNetworksFile = async(toolbox: any, network: string, dataSource: string, address: string, directory: string): Promise<void> => {
+  await toolbox.patching.update(path.join(directory, 'networks.json'), (config: any) => {
+    if(Object.keys(config).includes(network)) {
+      config[network][dataSource].address = address
+    } else {
+      config[network] = { [dataSource]: { address: address } }
+    }
+    return config
+  })
+}
+
+export const runCodegen = async (directory: string): Promise<boolean> => {
+  await graphCli.run(['codegen', path.join(directory, 'subgraph.yaml'), '-o',  path.join(directory, 'generated')])
+  return true
+}
+
+export const runBuild = async(network: string, directory: string): Promise<boolean> => {
+  await graphCli.run(['build', path.join(directory,'subgraph.yaml'), '-o', path.join(directory, 'build'), '--network', network, '--networkFile', path.join(directory,'networks.json')])
+  return true
+}
