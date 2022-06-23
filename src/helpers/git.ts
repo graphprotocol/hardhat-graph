095983bf4e33f58471941b3ad0a518cd9eb5f784
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-06-23 14:11:56 +0300

    Fix .gitignore lines

--- src/helpers/git.ts
+++ src/helpers/git.ts
@@ -46,12 +46,12 @@ export const initGitignore = async (toolbox: any, directory: string): Promise<bo
 
       const subgraphFilesIgnored = await toolbox.patching.exists('.gitignore', '# Subgraph')
       if (!subgraphFilesIgnored) {
-        await toolbox.patching.append('.gitignore', `# Subgraph\n${directory}/generated/\n${directory}/build/\n`)
+        await toolbox.patching.append('.gitignore', `# Subgraph\n${directory}/generated\n${directory}/build\n`)
       }
 
       const matchstickFilesIgnored = await toolbox.patching.exists('.gitignore', '# Matchstick')
       if (!matchstickFilesIgnored) {
-        await toolbox.patching.append('.gitignore', `# Matchstick\n${directory}/tests/.*/\n`)
+        await toolbox.patching.append('.gitignore', `# Matchstick\n${directory}/tests/.*\n`)
       }
 
       return true
