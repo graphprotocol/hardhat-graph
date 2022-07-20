339dc5073dbe4eb122cddcbf9a2c8c4f8afd5162
georg-getz <georgi.getz@outlook.com>
2022-07-20 10:08:14 +0300

    added a log for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import path from 'path'
 import "./type-extensions"
-import { extendConfig, experimentalAddHardhatNetworkMessageTraceHook} from "hardhat/config"
+import { extendConfig, experimentalAddHardhatNetworkMessageTraceHook } from "hardhat/config"
 import { config } from 'process'
 
 export * from "./tasks"
 
 extendConfig((config) => {
+  console.log("in extendConfig")
   if (!config.paths.subgraph) {
     config.paths.subgraph = './subgraph'
   }
